790e2dd08de57b5f7131cebf92408c9a0d9930ed
Angelina Argentieri <angelina.argentieri@aluno.senai.br>
2026-08-19 16:30:33 -0300

    Aula 19/08

--- PUBLIC/index.php
+++ PUBLIC/index.php
@@ -7,6 +7,10 @@ header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 
+
+
+
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit;
